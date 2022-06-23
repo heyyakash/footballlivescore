2a8d87b789ee9a04662b232d232792dd81ceac9b
heyyakash <akashsharma2002@gmail.com>
2022-06-23 22:29:04 +0530

    converted html to jsx

---
+++ src/Components/Home.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './HomeStyle.css';
+import logo from '../logo.svg';
+
+const Home = () => {
+  return (
+    <>
+            <div className="bar">
+        <ul>
+            <li><a href = "#"><img style={{width: "300px"}} src={logo} alt="PointBlank" /></a></li>
+            <li><a href = "#" id="heading">Live Score</a></li>
+          </ul>
+    </div>
+    <div className="scoreboard">
+        <table>
+            <tr>
+                <th id="Team1">
+                    Team 01
+                </th>
+                <th id="Team 2">
+                    Team 02
+                </th>
+            </tr>
+            <tr>
+                <td id="score1">
+                    00
+                </td>
+                <td id="score2">
+                    00
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    </>
+  )
+}
+
+export default Home
