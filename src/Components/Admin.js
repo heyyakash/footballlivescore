bfda4550b138c7a1ef5d194eb50b5ddc3864fe3c
heyyakash <akashsharma2002@gmail.com>
2022-06-23 22:43:24 +0530

    added react router and added style

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Admin = () => {
-  return (
-    <div>Admin</div>
-  )
+
+    const [team1, setTeam1] = useState("");
+    const [team2, setTeam2] = useState("");
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();   
+    }
+    return (
+        <div>
+            <input type="text" placeholder='team1' value={team1} onChange = {(e)=>setTeam1(e.target.value)} />
+            <input type="text" placeholder='team1' value={team2} onChange = {(e)=>setTeam2(e.target.value)} />
+            <button className='' type = "submit" onClick={(e)=>handleSubmit(e)}>Submit</button>
+        </div>
+    )
 }
 
 export default Admin
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,37 +3,37 @@ import './HomeStyle.css';
 import logo from '../logo.svg';
 
 const Home = () => {
-  return (
-    <>
+    return (
+        <div className='home'>
             <div className="bar">
-        <ul>
-            <li><a href = "#"><img style={{width: "300px"}} src={logo} alt="PointBlank" /></a></li>
-            <li><a href = "#" id="heading">Live Score</a></li>
-          </ul>
-    </div>
-    <div className="scoreboard">
-        <table>
-            <tr>
-                <th id="Team1">
-                    Team 01
-                </th>
-                <th id="Team 2">
-                    Team 02
-                </th>
-            </tr>
-            <tr>
-                <td id="score1">
-                    00
-                </td>
-                <td id="score2">
-                    00
-                </td>
-            </tr>
-        </table>
-    </div>
+                <ul>
+                    <li><a href="/"><img style={{ width: "300px" }} src={logo} alt="PointBlank" /></a></li>
+                    <li><a href="/" id="heading">Live Score</a></li>
+                </ul>
+            </div>
+            <div className="scoreboard">
+                <table>
+                    <tr>
+                        <th id="Team1">
+                            Team 01
+                        </th>
+                        <th id="Team 2">
+                            Team 02
+                        </th>
+                    </tr>
+                    <tr>
+                        <td id="score1">
+                            00
+                        </td>
+                        <td id="score2">
+                            00
+                        </td>
+                    </tr>
+                </table>
+            </div>
 
-    </>
-  )
+        </div>
+    )
 }
 
 export default Home
